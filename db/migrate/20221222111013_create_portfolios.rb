2d1f8c0e6bbf24d28282090376ade36d80394e34
Maximilidaddy <lohscheller1@gmail.com>
2022-12-22 12:31:44 +0100

    Added Portfolio items via Resource Generator

---
+++ db/migrate/20221222111013_create_portfolios.rb
@@ -0,0 +1,12 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+
+      t.timestamps
+    end
+  end
+end
