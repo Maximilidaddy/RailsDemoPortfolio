ed27e805e2293a838fba9d9fb8635d8de683f66b
Maximilidaddy <lohscheller1@gmail.com>
2023-01-02 12:59:30 +0100

    Created Seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+Blog.create!(
+	title: "My Blog Post #{blog}",
+	body: "My Name Is Cleveland Brown
+		And I Am Proud To Be
+		Right Back In My Home Town
+		With My New Family
+		There's Old Friends & New Friends & Even a Bear
+		Through Good Times & Bad Times
+		Its True Love To Share
+		And So I Found A Place
+		Where Everyone Will Know
+		My Happy Mustache Face
+		This Is The Cleveland Show!"
+	)
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilize: 15
+		)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+			title: "My Portfolio title: #{portfolio_item}",			
+			subtitle: "My great service",
+			body: "My Name Is Cleveland Brown
+				And I Am Proud To Be
+				Right Back In My Home Town
+				With My New Family
+				There's Old Friends & New Friends & Even a Bear
+				Through Good Times & Bad Times
+				Its True Love To Share
+				And So I Found A Place
+				Where Everyone Will Know
+				My Happy Mustache Face
+				This Is The Cleveland Show!",
+
+			main_image: "https://via.placeholder.com/150x350/O%20https://placeholder.com/",
+		)
+end
+	puts "9 portfolios created"
