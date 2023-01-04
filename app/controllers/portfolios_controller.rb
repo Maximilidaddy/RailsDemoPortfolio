39bdba87954cffb5ecdb6cb0e8e26e5a74ce9f6b
Maximilidaddy <lohscheller1@gmail.com>
2023-01-04 21:09:24 +0100

    implemented show function for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,6 +32,9 @@ class PortfoliosController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
-    end
+    end 
+  end
+  def show
+      @portfolio_item=Portfolio.find(params[:id])
   end
 end
