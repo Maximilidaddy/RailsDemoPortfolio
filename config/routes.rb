a8c09f09da079e67410d22dea2ddb3c9f3243e93
Maximilidaddy <lohscheller1@gmail.com>
2023-01-06 12:41:41 +0100

    Integrated Friendly Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+    #changing the route to ex: 'portfolio/5' -----> as: changes the prefix of the route in rake routes, change can be found in index file
     get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   
