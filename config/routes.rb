020f130cc86db9cd758fd8cfbffb0b38ea11e1f0
Maximilidaddy <lohscheller1@gmail.com>
2023-01-08 21:28:41 +0100

    implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
     #changing the route to ex: 'portfolio/5' -----> as: changes the prefix of the route in rake routes, change can be found in index file
+    
     get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
+    get 'angular-items', to: 'portfolios#angular'
   #changing route to just about and contact
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
