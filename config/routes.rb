50577250bc53bb6b95cf59a219baf94d0aaddede
Maximilidaddy <lohscheller1@gmail.com>
2023-01-10 20:59:45 +0100

    Integrated Basic Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
     #changing the route to ex: 'portfolio/5' -----> as: changes the prefix of the route in rake routes, change can be found in index file
     
