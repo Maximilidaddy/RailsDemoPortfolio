12881990725f837613186d1d3979c69d185b17f4
Maximilidaddy <lohscheller1@gmail.com>
2023-01-10 22:04:49 +0100

    Added Controller for Devise Params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+module DeviseWhitelist
+	extend ActiveSupport::Concern
+
+	included do 
+		#allows parameters to be added or removed for devise model
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	end
+	
+	def configure_permitted_parameters
+		#allows name parameter to be accepted in sign up feature, below does the same for edit
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	end
+end
